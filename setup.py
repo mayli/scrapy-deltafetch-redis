21e4d9155155951aeda18c53f06d3fdbb3edb232
Mengyang Li <mayli.he@gmail.com>
2017-01-25 22:12:12 -0800

    fixup setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='scrapy-deltafetch',
+    name='scrapy-deltafetch-redis',
     version='1.2.0',
     license='BSD',
-    description='Scrapy middleware to ignore previously crawled pages',
+    description='Scrapy middleware to ignore previously crawled pages redis',
     author='Scrapinghub',
     author_email='info@scrapinghub.com',
-    url='http://github.com/scrapy-plugins/scrapy-deltafetch',
-    packages=['scrapy_deltafetch'],
+    url='http://github.com/scrapy-plugins/scrapy-deltafetch-redis',
+    packages=['scrapy_deltafetch_redis'],
     platforms=['Any'],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -20,5 +20,5 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
     ],
-    install_requires=['Scrapy>=1.0', 'bsddb3']
+    install_requires=['Scrapy>=1.0', 'redis']
 )
